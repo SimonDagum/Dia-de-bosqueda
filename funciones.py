12268385aeb56cefb5e3eb1b9335d462e256e22b
SimonDagum <simonjdagum@gmail.com>
2025-08-10 21:34:09 -0300

    Update funciones.py
    cambios menores

--- funciones.py
+++ funciones.py
@@ -3,23 +3,24 @@ def ataquehacha(hacha,usoshacha):
     usoshacha=usoshacha-1
     if usoshacha==0:
         print(" Haz perdido tu hacha")
-        hacha=hacha-1
+        hacha=hacha-hacha
 def ataquelanza(lanza,usoslanza):
     print(" Eliminaste a tus oponentes con tu lanza")
     usoslanza=usoslanza-1
     if usoslanza==0:
         print(" Te quedaste sin lanza")
-        lanza=lanza-1
+        lanza=lanza-lanza
 def ataquenavaja(navaja,usosnavaja):
     print(" Apuñalaste a tu adversario con la navaja")
     usosnavaja=usosnavaja-1
     if usosnavaja==0:
         print(" La navaja fue destruida en su acto heroico")
-        navaja=navaja-1
+        navaja=navaja-navaja
 def ahuyentar(bengala):
     print(" Aquellos que se atrevieron a atacarte se vieron asustados por tu bengala")
-    bengala=bengala-1
+    bengala=bengala-bengala
 def dejarsemorir (salud,saludmaxima):
     print(" Debido a tus pobres instintos de supervivencia, dejaste que el enemigo tumbara tu cuerpo")
     salud=salud-saludmaxima
             salud=salud-saludmaxima
+
